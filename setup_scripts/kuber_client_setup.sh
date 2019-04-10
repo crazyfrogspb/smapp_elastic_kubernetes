16bf5c272a631037afb951afdf0375579658a973
Evgenii Nikitin <en919@nyu.edu>
2019-04-10 15:16:12 -0400

    change cpu limit

--- setup_scripts/kuber_client_setup.sh
+++ setup_scripts/kuber_client_setup.sh
@@ -6,5 +6,5 @@ sudo apt update && sudo apt install -qy docker-ce
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update && sudo apt-get install -y kubeadm kubelet kubectl
-sudo kubeadm join 192.168.0.71:6443 --token l5iifg.1v5eyryxlxu8heo7 \
-    --discovery-token-ca-cert-hash sha256:e27c10c9abb0ffc25d960c0aa6d5f34942ae566897ba2c42d4797895e5ecbeac
+sudo kubeadm join 192.168.0.71:6443 --token 01n01m.f6rr9pby1epwkyfw \
+    --discovery-token-ca-cert-hash sha256:4bb5a6964adb3cf75d41a050cef073ba37ed4a3c572dfd1c655cc4bcfb25e263
