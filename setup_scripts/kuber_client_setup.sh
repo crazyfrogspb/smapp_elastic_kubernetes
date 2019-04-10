8ca4a8f0aaa9633d2588c99855277d1f4bfeecb5
Evgenii Nikitin <en919@nyu.edu>
2019-04-10 16:42:47 -0400

    cluster setup

--- setup_scripts/kuber_client_setup.sh
+++ setup_scripts/kuber_client_setup.sh
@@ -6,8 +6,8 @@ sudo apt update && sudo apt install -qy docker-ce
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update && sudo apt-get install -y kubeadm kubelet kubectl
-sudo kubeadm join 192.168.0.124:6443 --token 4vbgxf.rt4m0182dd0zrzjg \
-    --discovery-token-ca-cert-hash sha256:8d3cb1891b1177ee1da747d59f201b8aad66378fa75f59e672209855d4c484db
+sudo kubeadm join 192.168.0.124:6443 --token 834y1y.9amv0vo23zb77vgw \
+    --discovery-token-ca-cert-hash sha256:372b8460676051653d6e254d1ac176006f77badf6e6fcabc645abb54ef50e160
 sudo systemctl daemon-reload
 sudo systemctl restart kubelet
 
--- setup_scripts/kuber_setup.sh
+++ setup_scripts/kuber_setup.sh
@@ -4,6 +4,8 @@ curl -s https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
 sudo apt install -qy docker-ce
+sudo groupadd docker
+sudo gpasswd -a $USER docker
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
@@ -14,8 +16,6 @@ sudo cp /etc/kubernetes/admin.conf $HOME/
 sudo chown $(id -u):$(id -g) $HOME/admin.conf
 export KUBECONFIG=$HOME/admin.conf
 kubectl -n kube-system apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
+kubectl taint nodes smapp-elastic-1 node-role.kubernetes.io/master-
 sudo systemctl daemon-reload
 sudo systemctl restart kubelet
-sudo groupadd docker
-sudo gpasswd -a $USER docker
-kubectl taint nodes smapp-elastic-1 node-role.kubernetes.io/master-
