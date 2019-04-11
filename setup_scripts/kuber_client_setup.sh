6bf1d1628f7f021a4aaa1f2cef49e549aec03dbb
Evgenii Nikitin <en919@nyu.edu>
2019-04-10 20:09:19 -0400

    increase probe periods

--- setup_scripts/kuber_client_setup.sh
+++ setup_scripts/kuber_client_setup.sh
@@ -6,8 +6,10 @@ sudo apt update && sudo apt install -qy docker-ce
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update && sudo apt-get install -y kubeadm kubelet kubectl
-sudo kubeadm join 192.168.0.124:6443 --token n01cqh.nj7d2di9s8mlwouj \
-    --discovery-token-ca-cert-hash sha256:ec5d0def75f81cdadf2b7ef221b196ff82dc481f465a5ab33e74507713161ee5
+sudo mkdir /usr/share/elasticsearch/
+sudo mkdir /usr/share/elasticsearch/data
+sudo kubeadm join 192.168.0.124:6443 --token mxmz9j.uidpb68tvvnfjos9 \
+    --discovery-token-ca-cert-hash sha256:f940b0987d5fb461468d4c83e783f446ce8dddd4e61edaa2a430310b8422f637
 sudo systemctl daemon-reload
 sudo systemctl restart kubelet
 
--- setup_scripts/kuber_setup.sh
+++ setup_scripts/kuber_setup.sh
@@ -6,6 +6,8 @@ sudo apt update
 sudo apt install -qy docker-ce
 sudo groupadd docker
 sudo gpasswd -a $USER docker
+sudo mkdir /usr/share/elasticsearch/
+sudo mkdir /usr/share/elasticsearch/data
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
