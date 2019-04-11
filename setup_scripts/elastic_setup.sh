2948e0dcdc06d4524402ccd8f0081bd4e44e5508
Evgenii Nikitin <en919@nyu.edu>
2019-04-11 16:05:42 -0400

    change ingress

--- setup_scripts/elastic_setup.sh
+++ setup_scripts/elastic_setup.sh
@@ -1,6 +1,7 @@
 cd cluster
 kubectl apply -f 1_k8s_global/
-#kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/mandatory.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/mandatory.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/provider/baremetal/service-nodeport.yaml
 kubectl apply -f 2_elasticsearch
 kubectl apply -f 3_kibana
 kubectl apply -f 4_ingress/static-ip-svc.yaml
