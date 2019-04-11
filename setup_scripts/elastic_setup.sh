3b829f06c8417fc806bb7e5739e212a2ab76316b
Evgenii Nikitin <en919@nyu.edu>
2019-04-11 14:30:12 -0400

    external ip

--- setup_scripts/elastic_setup.sh
+++ setup_scripts/elastic_setup.sh
@@ -1,6 +1,5 @@
 cd cluster
 kubectl apply -f 1_k8s_global/
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/mandatory.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/provider/cloud-generic.yaml
 kubectl apply -f 2_elasticsearch
 kubectl apply -f 3_kibana
